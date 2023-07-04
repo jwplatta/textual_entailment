96c01375f72b991524db6ef91db82a7991ecbcf2
Joseph Platta <jwplatta@gmail.com>
2023-07-04 13:12:37 -0700

    update imports in the initialization file

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,16 @@
 from .TextualEntailmentPrompter import TextualEntailmentPrompter
+from .BertEmbedder import BertEmbedder
+from .BertTextPreprocessor import BertTextPreprocessor
+from .NeuralNetwork import NeuralNetwork
+from .NeuralNetworkClassifier import NeuralNetworkClassifier
+from .SNLIFeatures import SNLIFeatures
+
 
 __all__ = [
-    "TextualEntailmentPrompter"
+    "TextualEntailmentPrompter",
+    "BertEmbedder",
+    "NeuralNetwork",
+    "NeuralNetworkClassifier",
+    "SNLIFeatures",
+    "BertTextPreprocessor"
 ]
