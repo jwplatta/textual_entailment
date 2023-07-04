9b13b1c8770f8b7dd688f1c9f404e7fe3eee1f87
Joseph Platta <jwplatta@gmail.com>
2023-07-04 13:10:08 -0700

    add a simple pytorch module with input layer, ReLU, and output layer

---
+++ src/NeuralNetwork.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+class NeuralNetwork(nn.Module):
+    """
+    A simple neural network with n_layers hidden layers and an output layer.
+    """
+    def __init__(self, in_features, out_features, n_classes) -> None:
+        super().__init__()
+        self.module_list = [
+          nn.Linear(
+            in_features,
+            out_features
+          ),
+          nn.ReLU(),
+          nn.Linear(out_features, n_classes)
+        ]
+
+        self.network = nn.Sequential(*self.module_list)
+
+
+    def forward(self, x):
+        """
+        Forward pass of the neural network.
+        """
+        return self.network(x)
